7bc1a6d91e460e93750e9ef0a3187148f44477c1
Filip Bozic <70634661+fbozic@users.noreply.github.com>
2025-03-11 12:06:59 +0100

    feat: keep contracts code only

--- crates/context/config/src/client/protocol.rs
+++
@@ -1,8 +0,0 @@
-pub mod icp;
-pub mod near;
-pub mod starknet;
-pub mod stellar;
-
-pub trait Protocol {
-    const PROTOCOL: &'static str;
-}
---
+++ scripts/build-rust.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Builds all rust components of the project
+# This script is intended to be run from the root of the project
+
+# Exit immediately if a command exits with a non-zero status.
+set -ex
+
+# Build contracts
+contracts/near/registry/build.sh
+contracts/near/context-config/build.sh
+contracts/near/context-proxy/build-test-deps.sh
+contracts/icp/context-config/build.sh
+contracts/icp/context-proxy/build_contracts.sh
+contracts/stellar/context-config/build.sh
+contracts/stellar/context-proxy/build_contracts.sh
+
+cargo build --all-targets --tests
