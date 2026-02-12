120e40b13509ec88535e516b0a3e09787a7ed7a8
Ronit <147048280+rtb-12@users.noreply.github.com>
2026-02-12 11:57:26 +0530

    feat(contracts): protocol cleanup (#42)
    * chore: remove Ethereum contracts and related files
    
    This commit removes the Ethereum contracts and associated build scripts, documentation, and CI configurations from the repository. The focus is now solely on NEAR contracts, streamlining the project structure and dependencies.
    
    * chore: update CI workflow and build scripts for NEAR contracts
    
    This commit simplifies the CI workflow by removing the matrix setup for protocols and focuses on NEAR contracts. It also updates the build script to explicitly build specified NEAR contracts, ensuring a more streamlined build process. Additionally, it removes unused dependencies from the Cargo.toml file.
    
    * chore: update Cargo.lock and CI workflows for NEAR contracts
    
    This commit updates the Cargo.lock file by removing outdated dependencies and adjusting versions for several packages. Additionally, it modifies the CI workflows to reflect the focus on NEAR contracts, renaming jobs and steps for clarity and consistency in the build process.

--- scripts/build-rust.sh
+++ scripts/build-rust.sh
@@ -4,13 +4,15 @@
 # This script is intended to be run from the root of the project
 
 # Exit immediately if a command exits with a non-zero status.
-set -ex
+set -euo pipefail
 
 # Build contracts
 contracts/near/registry/build.sh
 contracts/near/context-config/build.sh
 contracts/near/context-proxy/build-test-deps.sh
-contracts/icp/context-config/build.sh
-contracts/icp/context-proxy/build_contracts.sh
 
-RUSTFLAGS="--remap-path-prefix $HOME=~" cargo build --all-targets --tests
+RUSTFLAGS="--remap-path-prefix $HOME=~" cargo build --all-targets --tests \
+  -p calimero-registry \
+  -p calimero-context-config-near \
+  -p calimero-context-proxy-near \
+  -p calimero-mock-external-near
--- scripts/bump-contracts-version.sh
+++ scripts/bump-contracts-version.sh
@@ -32,7 +32,7 @@ fi
 new_version="$1"
 
 # Handle rust contracts
-rust_protocols=("icp" "near")
+rust_protocols=("near")
 for protocol in "${rust_protocols[@]}"; do
     echo "Updating version for protocol: ${protocol}"
     for contract in "contracts/${protocol}"/*; do
